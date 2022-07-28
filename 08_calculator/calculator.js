bb71119472b6db9cb43ec27dabe679a10ba56e9f
Jacob Feinland <feinlandj@gmail.com>
2022-07-28 14:39:12 -0400

    Complete calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,25 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(list) {
+  return list.reduce((a,b) => add(a,b), 0);
 };
 
-const multiply = function() {
-
+const multiply = function(list) {
+  return list.reduce((a,b) => a * b, 1);
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	return [...Array(n).keys()].reduce((a,b)=>a*(b+1), 1);
 };
 
 // Do not edit below this line

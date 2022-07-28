1d192ef9894655816c69b91e2ef1827f925b8e49
Jacob Feinland <feinlandj@gmail.com>
2022-07-28 15:06:23 -0400

    Complete exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
+function innerPalindromes(s) {
+    return (s.length <= 1) || (innerPalindromes(s.substring(1,s.length-1)) && (s.charAt(0) == s.charAt(s.length - 1)));
+}
 
+const palindromes = function (s) {
+    s = s.replace(/[.,!'":;><?|_{}-]/g, '').replace(/\s/g, '').toLowerCase();
+    return innerPalindromes(s);
 };
 
 // Do not edit below this line

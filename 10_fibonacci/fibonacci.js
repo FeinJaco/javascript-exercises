275983054f6adb4001563aa0222915bb0398294e
Jacob Feinland <feinlandj@gmail.com>
2022-07-28 15:15:50 -0400

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if (n < 0) return "OOPS";
+    let history1 = 1;
+    let history2 = 1;
+    for (let i = 3; i <= n; i++) {
+        let newVal = history1 + history2;
+        history1 = history2;
+        history2 = newVal;
+    }
+    return history2;
 };
 
 // Do not edit below this line

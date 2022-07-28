c9114399361d89231880871ee2acc7421c6f1b01
Jacob Feinland <feinlandj@gmail.com>
2022-07-28 16:14:21 -0400

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const CURRENT_YEAR = (new Date()).getFullYear();
+    const sorted = people.sort((a,b) => {
+        if (!a.yearOfDeath) a.yearOfDeath = CURRENT_YEAR;
+        if (!b.yearOfDeath) b.yearOfDeath = CURRENT_YEAR;
+        return (b.yearOfDeath - b.yearOfBirth) - (a.yearOfDeath - a.yearOfBirth);
+    });
+    return sorted[0];
 };
 
 // Do not edit below this line
